6bccb74204f62296aaf6ffa04ab61b80056a4c77
Ruhan <ruhancvbello@gmail.com>
2019-01-20 22:42:39 -0200

    Aula 1: Hello World

--- LocadoraPlatzi/Program.cs
+++ LocadoraPlatzi/Program.cs
@@ -10,6 +10,8 @@ namespace LocadoraPlatzi
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Hello World!");
+            Console.ReadLine();
         }
     }
 }
