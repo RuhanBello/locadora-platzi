74c2d68dfe02ea80c83fc4e200d1d70b7c8f2d0b
Ruhan <ruhancvbello@gmail.com>
2019-01-20 23:45:37 -0200

    Aula 3: Operacoes aritmeticas

--- LocadoraPlatzi/Program.cs
+++ LocadoraPlatzi/Program.cs
@@ -10,22 +10,64 @@ namespace LocadoraPlatzi
     {
         static void Main(string[] args)
         {
-            int valorIntA = 4;
-            int valorIntB;
-            valorIntB = 1;
+            Console.WriteLine("Inteiros");
 
-            int resultadoInt = valorIntA + valorIntB;
-            Console.WriteLine(resultadoInt);
-            
+            int valorIntA = 3;
+            int valorIntB = 2;
+            int resultadoInt;
+
+            resultadoInt = valorIntA + valorIntB;
+            Console.WriteLine("Inteiros " + valorIntA + " + " + valorIntB + " = " + resultadoInt);
+
+            resultadoInt = valorIntA - valorIntB;
+            Console.WriteLine("\nInteiros " + valorIntA + " - " + valorIntB + " = " + resultadoInt);
+
+            resultadoInt = valorIntA * valorIntB;
+            Console.WriteLine("\nInteiros " + valorIntA + " * " + valorIntB + " = " + resultadoInt);
+
+            resultadoInt = valorIntA / valorIntB;
+            Console.WriteLine("\nInteiros " + valorIntA + " / " + valorIntB + " = " + resultadoInt);
+
+            resultadoInt = valorIntA % valorIntB;
+            Console.WriteLine("\nInteiros " + valorIntA + " % " + valorIntB + " = " + resultadoInt);
+
+            resultadoInt = 0;
+            resultadoInt++;
+            Console.WriteLine("\nInteiro 0++ = " + resultadoInt);
+
+            resultadoInt = 0;
+            resultadoInt--;
+            Console.WriteLine("\nInteiro 0-- = " + resultadoInt);
+
+            resultadoInt = 0;
+            resultadoInt += 5;
+            Console.WriteLine("\nInteiro 0+=5 = " + resultadoInt);
+
+            resultadoInt = 0;
+            resultadoInt -= 5;
+            Console.WriteLine("\nInteiro 0-=5 = " + resultadoInt);
+
+            Console.Write("\n\nFloats");
             float valorFloatA = 1;
             float valorFloatB = 3;
-            float resultadoFloat = valorFloatA / valorFloatB;
-            Console.WriteLine(resultadoFloat);
+            float resultadoFloat;
 
+            resultadoFloat = valorFloatA * valorFloatB;
+            Console.WriteLine("\nFloats " + valorFloatA + " * " + valorFloatB + " = " + resultadoFloat);
+
+            resultadoFloat = valorFloatA / valorFloatB;
+            Console.WriteLine("\nFloats " + valorFloatA + " / " + valorFloatB + " = " + resultadoFloat);
+
+            Console.Write("\n\nDoubles");
             double valorDoubleA = 1;
             double valorDoubleB = 3;
-            double resultadoDouble = valorDoubleA / valorDoubleB;
-            Console.WriteLine(resultadoDouble);
+            double resultadoDouble;
+
+            resultadoDouble = valorDoubleA * valorDoubleB;
+            Console.WriteLine("\nDoubles " + valorDoubleA + " * " + valorDoubleB + " = " + resultadoDouble);
+
+            resultadoDouble = valorDoubleA / valorDoubleB;
+            Console.WriteLine("\nDoubles " + valorDoubleA + " / " + valorDoubleB + " = " + resultadoDouble);
 
             Console.ReadLine();
         }
