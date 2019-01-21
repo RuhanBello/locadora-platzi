1c84946a9e34ff74104bdeac7cd5e5e771aa4d7d
Ruhan <ruhancvbello@gmail.com>
2019-01-20 23:24:33 -0200

    Aula 2: Variaveis e tipos int, float e double

--- LocadoraPlatzi/Program.cs
+++ LocadoraPlatzi/Program.cs
@@ -10,7 +10,23 @@ namespace LocadoraPlatzi
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int valorIntA = 4;
+            int valorIntB;
+            valorIntB = 1;
+
+            int resultadoInt = valorIntA + valorIntB;
+            Console.WriteLine(resultadoInt);
+            
+            float valorFloatA = 1;
+            float valorFloatB = 3;
+            float resultadoFloat = valorFloatA / valorFloatB;
+            Console.WriteLine(resultadoFloat);
+
+            double valorDoubleA = 1;
+            double valorDoubleB = 3;
+            double resultadoDouble = valorDoubleA / valorDoubleB;
+            Console.WriteLine(resultadoDouble);
+
             Console.ReadLine();
         }
     }
