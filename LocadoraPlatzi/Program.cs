bbd211eb8f393ff3aa347e0ba5f5e500b3be15a0
Ruhan <ruhancvbello@gmail.com>
2019-01-21 00:39:57 -0200

    Aula 9: while

--- LocadoraPlatzi/Program.cs
+++ LocadoraPlatzi/Program.cs
@@ -29,38 +29,17 @@ namespace LocadoraPlatzi
             unicoFilme.quantidade = 5;
             unicoFilme.lancamento = false;
 
-            Console.WriteLine("Seja bem-vindo à locadora Platzi!");
-            Console.WriteLine("Por enquanto, este é o nosso único filme:");
-
-            Console.WriteLine("Nome: " + unicoFilme.nome);
-            Console.WriteLine("Ano de lançamento: " + unicoFilme.anoLancamento);
-            Console.WriteLine("Gênero: " + unicoFilme.genero);
-            Console.WriteLine("Avaliação: " + unicoFilme.avaliacao);
-            Console.WriteLine("Quantidade: " + unicoFilme.quantidade);
-            Console.WriteLine("Lançamento: " + unicoFilme.lancamento);
-
-            Console.WriteLine("\n");
-
-            if (unicoFilme.lancamento)
+            while (true)
             {
-                Console.WriteLine("O filme é um lançamento");
-            } else
-            {
-                Console.WriteLine("O filme não é um lançamento");
+                Console.Clear();
+                Console.WriteLine("Seja bem vindo à locadora de filmes Platzi!");
+                Console.WriteLine("Escolha uma opção:");
+                Console.WriteLine("1 - Listar todos os filmes");
+                Console.WriteLine("0 - Sair");
+                Console.WriteLine("\n");
+
+                Console.ReadLine();
             }
-
-            if(unicoFilme.avaliacao < 7)
-            {
-                Console.WriteLine("O filme não é tão bom.");
-            }else if(unicoFilme.avaliacao >= 7 && unicoFilme.avaliacao < 9)
-            {
-                Console.WriteLine("O filme é bom!");
-            } else
-            {
-                Console.WriteLine("O filme é fantástico!");
-            }
-
-            Console.ReadLine();
         }
     }
 }
